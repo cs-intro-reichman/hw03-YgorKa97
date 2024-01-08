772f97520071c1fd0cf516b4b17547a5b80ee7ee
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2024-01-08 17:02:33 +0200

    Update lowercase.java

--- lowercase.java
+++ lowercase.java
@@ -1,5 +1,5 @@
 /** String processing exercise 1. */
-public class LowerCase {
+public class lowerCase {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(lowerCase(str));
