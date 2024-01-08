468eeb9707aa996d286c4a5c22f66ec93d769174
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2024-01-08 17:02:45 +0200

    Update uniquechars.java

--- uniquechars.java
+++ uniquechars.java
@@ -1,5 +1,5 @@
 /** String processing exercise 2. */
-public class UniqueChars {
+public class uniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(uniqueChars(str));
